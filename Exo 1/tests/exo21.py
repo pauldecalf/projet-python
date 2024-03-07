dd56f088f7b67b47d23c06123009d69a46b672c9
pauldecalf <70703497+pauldecalf@users.noreply.github.com>
2024-03-07 20:15:31 +0100

    create exo and tests

--- Exo 1/tests/exo21.py
+++
@@ -1,9 +0,0 @@
-# Écrivez un programme qui construit un arbre binaire et imprime ses éléments en ordre préfixé.
-
-def pre_order_traversal(tree):
-    if tree:
-        print(tree[0], end=' ')
-        pre_order_traversal(tree[1])
-        pre_order_traversal(tree[2])
-
-
--- Exo 1/tests/test_exo21.py
+++ Exo 1/tests/test_exo21.py
@@ -1,48 +1,25 @@
-# test de # Écrivez un programme qui construit un arbre binaire et imprime ses éléments en ordre préfixé.
+# test de # Créer une application Python qui utilise la reconnaissance optique de caractères (OCR) pour lire des énigmes à partir d'images et fournir les réponses correspondantes.
 #
-# def pre_order_traversal(tree):
-#     if tree:
-#         print(tree[0], end=' ')
-#         pre_order_traversal(tree[1])
-#         pre_order_traversal(tree[2])
+# def extract_string_details(phrase):
+#     return {
+#         'first_6': phrase[:6],
+#         'last_9': phrase[-9:],
+#         'slice_13_38': phrase[13:38].lstrip(),
+#         'reversed': phrase[::-1]
+#     }
 
-from exo21 import pre_order_traversal
-def test_pre_order_traversal(capsys):
-    tree = ['A', ['B', ['D', [], []], ['E', [], []]], ['C', ['F', [], []], []]]
-    pre_order_traversal(tree)
-    captured = capsys.readouterr()
-    assert captured.out == 'A B D E C F '
-    tree = ['A', ['B', [], []], ['C', [], []]]
-    pre_order_traversal(tree)
-    captured = capsys.readouterr()
-    assert captured.out == 'A B C '
-    tree = ['A', [], []]
-    pre_order_traversal(tree)
-    captured = capsys.readouterr()
-    assert captured.out == 'A '
-    tree = []
-    pre_order_traversal(tree)
-    captured = capsys.readouterr()
-    assert captured.out == ''
-    tree = ['A', ['B', [], []], ['C', [], []]]
-    pre_order_traversal(tree)
-    captured = capsys.readouterr()
-    assert captured.out == 'A B C '
-    tree = ['A', ['B', ['D', [], []], ['E', [], []]], ['C', ['F', [], []], []]]
-    pre_order_traversal(tree)
-    captured = capsys.readouterr()
-    assert captured.out == 'A B D E C F '
-    tree = ['A', ['B', ['D', [], []], ['E', [], []]], ['C', ['F', [], []], []]]
-    pre_order_traversal(tree)
-    captured = capsys.readouterr()
-    assert captured.out == 'A B D E C F '
-    tree = ['A', ['B', ['D', [], []], ['E', [], []]], ['C', ['F', [], []], []]]
-    pre_order_traversal(tree)
-    captured = capsys.readouterr()
-    assert captured.out == 'A B D E C F '
-    tree = ['A', ['B', ['D', [], []], ['E', [], []]], ['C', ['F', [], []], []]]
-    pre_order_traversal(tree)
-    captured = capsys.readouterr()
-    assert captured.out == 'A B D E C F '
-    tree = ['A', ['B', ['D', [], []], ['E', [], []]], ['C', ['F', [], []], []]]
-    pre_order_traversal(tree)
+from exo21 import extract_string_details
+
+def test_extract_string_details():
+    phrase = "Python est un langage de programmation puissant et facile à apprendre"
+    expected_results = {
+        'first_6': "Python",
+        'last_9': "apprendre",  # Assurez-vous que ceci correspond à la fin de votre chaîne 'phrase'.
+        'slice_13_38': " langage de programmation",  # Supprimez l'espace au début si votre chaîne commence directement par 'langage'
+        'reversed': "erdnerppa à elicaf te tnassiup noitammargorp ed egagnal nu tse nohtyP",  # Assurez-vous que cette chaîne est exactement l'inverse de 'phrase'.
+    }
+
+    results = extract_string_details(phrase)
+
+    for key in expected_results:
+        assert results[key] == expected_results[key], f"Erreur dans {key}"
