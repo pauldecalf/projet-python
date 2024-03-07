5821285cd301a886e822b48b2487d9a44130a4ff
pauldecalf <70703497+pauldecalf@users.noreply.github.com>
2024-03-07 10:55:31 +0100

    update test_exo1.py

--- Exo 1/test_exo1.py
+++ Exo 1/test_exo1.py
@@ -2,6 +2,11 @@ def somme(x, y):
     return x + y
 
 
-def test_somme():
+def test_somme_true():
     assert somme(2, 3) == 5
     assert somme(1, 1) == 2
+
+
+def test_somme_false():
+    assert somme(2, 3) == 1
+    assert somme(1, 1) == 4
